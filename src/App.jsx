38535c309a5c1efa70baa76b3ff6e716873bdf7a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-07-23 11:30:53 +0700

    Complete tutorial: My First Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,108 @@
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css' // Mandatory CSS required by the Data Grid
 import 'ag-grid-community/styles/ag-theme-quartz.css' // Optional Theme applied to the Data Grid
-import { useState } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 export default function App() {
   const CustomButtonComponent = (props) => {
     return <button onClick={() => window.alert('clicked')}>Push Me!</button>
   }
   // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false }
-  ])
+  const [rowData, setRowData] = useState([])
+
+  // Fetch data & update rowData state
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/space-mission-data.json') // Fetch data from server
+      .then((result) => result.json()) // Convert to JSON
+      .then((rowData) => setRowData(rowData)) // Update state of `rowData`
+  }, [])
+
+  // Custom Cell Renderer (Display flags based on cell value)
+  const CompanyLogoRenderer = (props) => {
+    const value = props.data.company
+    return (
+      <span
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          alignItems: 'center'
+        }}
+      >
+        {value && (
+          <img
+            alt={`${value} Flag`}
+            src={`https://www.ag-grid.com/example-assets/space-company-logos/${value.toLowerCase()}.png`}
+            style={{
+              display: 'block',
+              width: '25px',
+              height: 'auto',
+              maxHeight: '50%',
+              marginRight: '12px',
+              filter: 'brightness(1.1)'
+            }}
+          />
+        )}
+        <p
+          style={{
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap'
+          }}
+        >
+          {value}
+        </p>
+      </span>
+    )
+  }
+
+  const SuccessfullIconRenderer = (props) => {
+    console.log(props.value)
+    return (
+      <span width="100%" height="100%">
+        {props.value ? '✅' : '❌'}
+      </span>
+    )
+  }
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState([
     {
-      field: 'make',
-      flex: 2,
-      filter: true,
-      floatingFilter: true,
-      editable: true
+      field: 'mission',
+      cellRenderer: CompanyLogoRenderer,
+      checkboxSelection: true
     },
-    { field: 'model' },
-    { field: 'price', valueFormatter: (p) => '£' + p.value.toLocaleString() },
-    { field: 'electric' },
+    { field: 'company' },
+    { field: 'location' },
     {
-      headerName: 'Make & Model',
-      valueGetter: (p) => p.data.make + ' ' + p.data.model
+      field: 'date',
+      valueFormatter: (params) => {
+        const date = new Date(params.value)
+        return date.toLocaleDateString('id-ID', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }
     },
-    { field: 'button', cellRenderer: CustomButtonComponent }
+    {
+      field: 'price',
+      valueFormatter: (params) => {
+        return '£' + params.value.toLocaleString()
+      }
+    },
+    { field: 'successful', cellRenderer: SuccessfullIconRenderer },
+    { field: 'rocket' }
   ])
+
+  const defaultColDef = useMemo(
+    () => ({
+      filter: true, // Enable filtering on all columns
+      editable: true
+    }),
+    []
+  )
 
   return (
     <div
@@ -43,8 +114,13 @@ export default function App() {
         columnDefs={colDefs}
         rowSelection="multiple"
         pagination={true}
-        paginationPageSize={1}
-        paginationPageSizeSelector={[1, 2, 3]}
+        paginationPageSize={20}
+        paginationPageSizeSelector={[20, 50, 100]}
+        defaultColDef={defaultColDef}
+        onCellValueChanged={(event) =>
+          console.log(`New Cell Value: ${event.value}`)
+        }
+        onSelectionChanged={(event) => console.log(event)}
       />
     </div>
   )
